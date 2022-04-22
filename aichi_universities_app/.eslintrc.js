4a50a24223bb363219212b83a82f680a5de38d6e
Yamamori <moriyama14t@gmail.com>
2022-04-22 05:36:21 +0000

    画面作成並びにmicroCMS連携

--- aichi_universities_app/.eslintrc.js
+++ aichi_universities_app/.eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   },
 
   //プラグイン導入
-  extends: ['plugin:vue/recommended', 'eslint:recommended', '@vue/prettier'], //typescriptも必要なら '@vue/typescript' を追記
+  extends: ['plugin:vue/recommended', 'eslint:recommended', '@vue/typescript'], //typescriptも必要なら '@vue/typescript' を追記
 
   // typescriptを読み込む
   // parserOptions: {
@@ -25,11 +25,9 @@ module.exports = {
     'vue/component-definition-name-casing': 'warn',
     'vue/component-name-in-template-casing': 'warn',
     'vue/component-tags-order': ['error', { order: ['template', 'script', 'style'] }],
-    'vue/no-deprecated-scope-attribute': 'warn', //2.6.0以降では非推奨の書き方
-    'vue/no-deprecated-slot-attribute': 'warn', //2.6.0以降では非推奨の書き方
-    'vue/no-deprecated-slot-scope-attribute': 'warn', //2.6.0以降では非推奨の書き方
     'vue/v-on-function-call': 'warn',
     'vue/v-slot-style': 'warn',
     'vue/valid-v-slot': 'error',
+    'vue/multi-word-component-names': 0,
   },
 }
--- aichi_universities_app/src/router/index.ts
+++ aichi_universities_app/src/router/index.ts
@@ -1,24 +1,23 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Universities from "../views/Universities.vue";
+import UniversityDetail from "@/views/UniversityDetail.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "universities",
+    component: Universities,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/:id",
+    name: "UniversityDetail",
+    component: UniversityDetail,
   },
+
+
 ];
 
 const router = new VueRouter({
